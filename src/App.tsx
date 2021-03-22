4d040d0aac19ee1f7cf3e6cf2ae177e5cf2611d8
Kesley David <kesleygti@gmail.com>
2021-03-22 08:45:38 -0300

    [web] Add Styled Components and start styles global

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { MovieCard } from './components/MovieCard';
 
 import { api } from './services/api';
 
-import './styles/global.scss';
+import { GlobalStyle } from "./styles/global";
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
@@ -58,37 +58,40 @@ export function App() {
   }
   
   return (
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
-      {/* <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
+    <>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <nav className="sidebar">
+          <span>Watch<p>Me</p></span>
 
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              id={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav> */}
-
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          <div className="buttons-container">
+            {genres.map(genre => (
+              <Button
+                id={String(genre.id)}
+                title={genre.title}
+                iconName={genre.name}
+                onClick={() => handleClickButton(genre.id)}
+                selected={selectedGenreId === genre.id}
+              />
             ))}
           </div>
-        </main>
+
+        </nav>
+
+        <div className="container">
+          <header>
+            <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+          </header>
+
+          <main>
+            <div className="movies-list">
+              {movies.map(movie => (
+                <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+              ))}
+            </div>
+          </main>
+        </div>
       </div>
-    </div>
+      <GlobalStyle />
+    </>
   )
 }
